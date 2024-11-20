90fa1cb1e6d6db0052d173739d9163a9aa909b49
Brownson <esitibrownson@gmail.com>
2024-11-20 14:13:56 +0100

    c

--- src/validator/statement.validator.ts
+++ src/validator/statement.validator.ts
@@ -8,7 +8,8 @@ import {
 @ValidatorConstraint({ name: 'fxqlValidator', async: false })
 export class FxqlValidator implements ValidatorConstraintInterface {
   validate(fxql: string, args: ValidationArguments) {
-    const regex = /([A-Z]{3}-[A-Z]{3}) \{\n\s*BUY ([1-9]\d{0,2}(\.\d+)?)\n\s*SELL ([1-9]\d{0,2}(\.\d+)?)\n\s*CAP (0|[1-9]\d*)\n\}/g;
+    const regex =
+      /([A-Z]{3}-[A-Z]{3}) \{\\n\s*BUY ([1-9]\d(\.\d+)?)\\n\s*SELL ([1-9]\d(\.\d+)?)\\n\s*CAP (0|[1-9]\d*)\\n\}/g;
     const matches = fxql.match(regex);
     return matches !== null && matches.length === fxql.split('}').length - 1;
   }
@@ -17,4 +18,3 @@ export class FxqlValidator implements ValidatorConstraintInterface {
     return 'FXQL must contain valid blocks in the correct format: USD-GBP {\\n BUY 100.50\\n SELL 200\\n CAP 93800\\n}.';
   }
 }
-
